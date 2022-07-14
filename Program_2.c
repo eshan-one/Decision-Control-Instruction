5757fc647145d5a9ea45a8624af2deb980e01312
eshan-one <108228113+eshan-one@users.noreply.github.com>
2022-07-14 23:05:38 +0530

    Divisibility by 5

---
+++ Program_2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    int x;
+    printf("Enter a number: \n");
+    scanf("%d", &x);
+    if (x % 5 == 0)
+    {
+        printf("Number is divisible by 5");
+    }
+
+    else
+    {
+        printf("Number is not divisible by 5");
+    }
+
+    return 0;
+}
